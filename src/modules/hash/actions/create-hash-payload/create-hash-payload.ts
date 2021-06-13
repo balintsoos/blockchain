33400b41da7f2d241998c2037b7f9753a4349ebc
Balint Soos <soosbalint95@gmail.com>
2021-06-13 22:53:47 +0200

    delete modules folder

--- src/modules/hash/actions/create-hash-payload/create-hash-payload.ts
+++
@@ -1,5 +0,0 @@
-export type CreateHashPayload = (data: object, previousHash: string, timestamp: string) => string;
-
-export const createCreateHashPayload = (): CreateHashPayload => (data, previousHash, timestamp) => {
-  return `${timestamp}#${previousHash}#${JSON.stringify(data)}`;
-};
--- src/modules/hash/actions/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './calculate-hash';
-export * from './create-hash-payload';
-export * from './create-hash';
